44da384d79835facf318a96e2fbdd96a7c1cb254
namdev1994 <n.lavhare1994@gmail.com>
2021-05-05 15:27:12 +0530

    Added : Chose Player Letter

--- src/tictactoegame/TicTacToeGame.java
+++ src/tictactoegame/TicTacToeGame.java
@@ -11,9 +11,27 @@ public class TicTacToeGame {
 		}
 		return board;
 	}
+	public char playerChoice() //method creation
+	{
+		char player,computer;
+		System.out.println("Enter player choice");
+		player=sc.next().charAt(0); //Accepting letter from plyer	
+		if(player=='x')
+		{
+			player='x';
+			computer='0';
+		}
+		else if(player=='0')
+		{
+			player='0';
+			computer='x';
+		}
+		return player;
+	}
 	public static void main(String[] args) {
 		TicTacToeGame tictactoe=new TicTacToeGame();//object creation
 		tictactoe.createBoard();//method call
+		tictactoe.playerChoice(); // method call
 	}
 
 }
