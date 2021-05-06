ac7302679fc2d4763603decbf8d799cb5e752655
namdev1994 <n.lavhare1994@gmail.com>
2021-05-06 12:16:58 +0530

    Added:  Check to desired location Is free

--- src/tictactoegame/TicTacToeGame.java
+++ src/tictactoegame/TicTacToeGame.java
@@ -73,6 +73,97 @@ public class TicTacToeGame {
 				System.out.println("Enetr valid place");
 		}
 	}
+	public void checkDesiredLocationIsFree() //desired location method
+	{
+		System.out.println("Enter desired location ");
+		int location=sc.nextInt(); //take input from user for desired location
+		switch(location)
+		{
+		case 1:
+		    if(board[1]==' ')
+		    {
+			board[1]='x';
+		    }
+		    else{
+		        System.out.println("Enetr at valid place");
+		    }
+			break;
+		case 2:
+		    if(board[2]==' ')
+		    {
+			board[2]='x';
+		    }
+		    else{
+		        System.out.println("Enetr at valid place");
+		    }
+			break;
+		case 3:
+		    if(board[3]==' ')
+		    {
+			board[3]='x';
+		    }
+		    else{
+		        System.out.println("Enetr at valid place");
+		    }
+			break;
+		case 4:
+		    if(board[4]==' ')
+		    {
+			board[4]='x';
+		    }
+		    else{
+		        System.out.println("Enetr at valid place");
+		    }
+			break;
+		case 5:
+		    if(board[5]==' ')
+		    {
+			board[5]='x';
+		    }
+		    else{
+		        System.out.println("Enetr at valid place");
+		    }
+			break;
+		case 6:
+		    if(board[6]==' ')
+		    {
+			board[6]='x';
+		    }
+		    else{
+		        System.out.println("Enetr at valid place");
+		    }
+			break;
+		case 7:
+		    if(board[7]==' ')
+		    {
+			board[7]='x';
+		    }
+		    else{
+		        System.out.println("Enetr at valid place");
+		    }
+			break;
+		case 8:
+		    if(board[8]==' ')
+		    {
+			board[8]='x';
+		    }
+		    else{
+		        System.out.println("Enetr at valid place");
+		    }
+			break;
+		case 9:
+		    if(board[9]==' ')
+		    {
+			board[9]='x';
+		    }
+		    else{
+		        System.out.println("Enetr at valid place");
+		    }
+			default :
+				System.out.println("Enetr valid place");
+		}
+		
+	}
 	
 	public static void main(String[] args) {
 		TicTacToeGame tictactoe=new TicTacToeGame();//object creation
@@ -80,6 +171,7 @@ public class TicTacToeGame {
 		tictactoe.playerChoice(); // method call
 		tictactoe.showBoard(); // showBoard Method call
 		tictactoe.moveToDesiredLocation(); //desired Location method call
+		tictactoe.checkDesiredLocationIsFree(); // method call to check Location is free 
 		
 	}
 
