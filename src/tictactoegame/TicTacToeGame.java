94ae7b726101d9e7d474dedd0f97b26e9ef58997
namdev1994 <n.lavhare1994@gmail.com>
2021-05-07 00:38:13 +0530

    Refactor: Correcton are done in master

--- src/tictactoegame/TicTacToeGame.java
+++ src/tictactoegame/TicTacToeGame.java
@@ -1,19 +1,11 @@
 package tictactoegame;
 import java.util.*;
 public class TicTacToeGame {
-<<<<<<< HEAD
-	Scanner sc=new Scanner(System.in);
-	char player,computer;
-	static int location;
-	static boolean isUserPlay;
-	 char[]board=new char[10];//Array
-=======
 	static Scanner sc=new Scanner(System.in);
 	static int location,indexCount;
 	static boolean userPlay;
 	static char player,computer;
 	static char[]board=new char[10];//Array
->>>>>>> UC7_CheckWinnerOrTie
 	public char[] createBoard()   //method Implementation
 	{
 		for(int i=1;i<board.length;i++)
@@ -26,7 +18,7 @@ public class TicTacToeGame {
 	{
 		System.out.println("Enter player choice");
 		player=sc.next().charAt(0); //Accepting letter from plyer	
-		/*if(player=='x')
+		if(player=='x')
 		{
 			player='x';
 			computer='0';
@@ -35,7 +27,7 @@ public class TicTacToeGame {
 		{
 			player='0';
 			computer='x';
-		}*/
+		}
 		return player;
 	}
 	public static void showBoard() //ShowBoard Method 
@@ -46,11 +38,7 @@ public class TicTacToeGame {
 		System.out.println("-+-+-");
 		System.out.println(board[7]+"|"+board[8]+"|"+board[9]);	
 	}
-<<<<<<< HEAD
-	public int moveToDesiredLocation() //desired location method
-=======
 	public static void moveToDesiredLocation() //desired location method
->>>>>>> UC7_CheckWinnerOrTie
 	{
 		System.out.println("Enter desired location ");
 		int location=sc.nextInt(); //take input from user for desired location
@@ -87,121 +75,11 @@ public class TicTacToeGame {
 				System.out.println("Enetr valid place");
 		}
 		showBoard();
-<<<<<<< HEAD
-		return location;
-	}
-	public int checkDesiredLocationIsFree() //desired location method
-=======
 	}
 	public static void makeUserMove() //desired location method
->>>>>>> UC7_CheckWinnerOrTie
 	{
 		if (indexCount == 9)
 		{
-<<<<<<< HEAD
-		case 1:
-		    if(board[1]==' ')
-		    {
-			board[1]=player;
-		    }
-		    else{
-		        System.out.println("Enetr at valid place");
-		    }
-			break;
-		case 2:
-		    if(board[2]==' ')
-		    {
-			board[2]=player;
-		    }
-		    else{
-		        System.out.println("Enetr at valid place");
-		    }
-			break;
-		case 3:
-		    if(board[3]==' ')
-		    {
-			board[3]=player;
-		    }
-		    else{
-		        System.out.println("Enetr at valid place");
-		    }
-			break;
-		case 4:
-		    if(board[4]==' ')
-		    {
-			board[4]=player;
-		    }
-		    else{
-		        System.out.println("Enetr at valid place");
-		    }
-			break;
-		case 5:
-		    if(board[5]==' ')
-		    {
-			board[5]=player;
-		    }
-		    else{
-		        System.out.println("Enetr at valid place");
-		    }
-			break;
-		case 6:
-		    if(board[6]==' ')
-		    {
-			board[6]=player;
-		    }
-		    else{
-		        System.out.println("Enetr at valid place");
-		    }
-			break;
-		case 7:
-		    if(board[7]==' ')
-		    {
-			board[7]=player;
-		    }
-		    else{
-		        System.out.println("Enetr at valid place");
-		    }
-			break;
-		case 8:
-		    if(board[8]==' ')
-		    {
-			board[8]=player;
-		    }
-		    else{
-		        System.out.println("Enetr at valid place");
-		    }
-			break;
-		case 9:
-		    if(board[9]==' ')
-		    {
-			board[9]=player;
-		    }
-		    else{
-		        System.out.println("Enetr at valid place");
-		    }
-			default :
-				System.out.println("Enetr valid place");
-		}
-		
-		showBoard();
-		return location;
-		}
-		
-	public int checkToss() //checkToss to who play first
-	{
-	    Random random=new Random();
-	    int checktoss=random.nextInt(2)+1;
-	    if(checktoss==1)
-	    {
-	        System.out.println("User Start First");
-	        isUserPlay=true;  
-	    }
-	    else{
-	        System.out.println("Computer Start First");
-	    }
-	    return checktoss;
-	}
-=======
 			System.out.println("played all turns");
 			System.exit(0);
 		}
@@ -304,20 +182,12 @@ public class TicTacToeGame {
             System.exit(0);
         }
     }
->>>>>>> UC7_CheckWinnerOrTie
 	
 	public static void main(String[] args) {
 		TicTacToeGame tictactoe=new TicTacToeGame();//object creation
 		tictactoe.createBoard();//method call
 		tictactoe.playerChoice(); // method call
 		tictactoe.showBoard(); // showBoard Method call
-<<<<<<< HEAD
-		tictactoe.moveToDesiredLocation(); //desired Location method call
-		while(location<10)
-		{
-		tictactoe.checkDesiredLocationIsFree(); //desired location method
-		}
-=======
 		tictactoe.checkToss();
 		while(indexCount < 9)
 		{
@@ -326,7 +196,6 @@ public class TicTacToeGame {
 			tictactoe.winnerOrTie();
 		}
 		
->>>>>>> UC7_CheckWinnerOrTie
 	}
-}
 
+}
