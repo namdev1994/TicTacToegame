9fa2ec1ea16e9afbbc75c7ebc9b2e2984b79a170
namdev1994 <n.lavhare1994@gmail.com>
2021-05-06 18:49:42 +0530

    Added :check Toss

--- src/tictactoegame/TicTacToeGame.java
+++ src/tictactoegame/TicTacToeGame.java
@@ -2,6 +2,9 @@ package tictactoegame;
 import java.util.*;
 public class TicTacToeGame {
 	Scanner sc=new Scanner(System.in);
+	char player,computer;
+	static int location;
+	static boolean isUserPlay;
 	 char[]board=new char[10];//Array
 	public char[] createBoard()   //method Implementation
 	{
@@ -13,10 +16,9 @@ public class TicTacToeGame {
 	}
 	public char playerChoice() //method creation
 	{
-		char player,computer;
 		System.out.println("Enter player choice");
 		player=sc.next().charAt(0); //Accepting letter from plyer	
-		if(player=='x')
+		/*if(player=='x')
 		{
 			player='x';
 			computer='0';
@@ -25,7 +27,7 @@ public class TicTacToeGame {
 		{
 			player='0';
 			computer='x';
-		}
+		}*/
 		return player;
 	}
 	public void showBoard() //ShowBoard Method 
@@ -36,44 +38,46 @@ public class TicTacToeGame {
 		System.out.println("-+-+-");
 		System.out.println(board[7]+"|"+board[8]+"|"+board[9]);	
 	}
-	public void moveToDesiredLocation() //desired location method
+	public int moveToDesiredLocation() //desired location method
 	{
 		System.out.println("Enter desired location ");
 		int location=sc.nextInt(); //take input from user for desired location
 		switch(location)
 		{
 		case 1:
-			board[1]='x';
+			board[1]=player;
 			break;
 		case 2:
-			board[2]='x';
+			board[2]=player;
 			break;
 		case 3:
-			board[3]='x';
+			board[3]=player;
 			break;
 		case 4:
-			board[4]='x';
+			board[4]=player;
 			break;
 		case 5:
-			board[5]='x';
+			board[5]=player;
 			break;
 		case 6:
-			board[6]='x';
+			board[6]=player;
 			break;
 		case 7:
-			board[7]='x';
+			board[7]=player;
 			break;
 		case 8:
-			board[8]='x';
+			board[8]=player;
 			break;
 		case 9:
-			board[9]='x';
+			board[9]=player;
 			break;
 			default :
 				System.out.println("Enetr valid place");
 		}
+		showBoard();
+		return location;
 	}
-	public void checkDesiredLocationIsFree() //desired location method
+	public int checkDesiredLocationIsFree() //desired location method
 	{
 		System.out.println("Enter desired location ");
 		int location=sc.nextInt(); //take input from user for desired location
@@ -82,7 +86,7 @@ public class TicTacToeGame {
 		case 1:
 		    if(board[1]==' ')
 		    {
-			board[1]='x';
+			board[1]=player;
 		    }
 		    else{
 		        System.out.println("Enetr at valid place");
@@ -91,7 +95,7 @@ public class TicTacToeGame {
 		case 2:
 		    if(board[2]==' ')
 		    {
-			board[2]='x';
+			board[2]=player;
 		    }
 		    else{
 		        System.out.println("Enetr at valid place");
@@ -100,7 +104,7 @@ public class TicTacToeGame {
 		case 3:
 		    if(board[3]==' ')
 		    {
-			board[3]='x';
+			board[3]=player;
 		    }
 		    else{
 		        System.out.println("Enetr at valid place");
@@ -109,7 +113,7 @@ public class TicTacToeGame {
 		case 4:
 		    if(board[4]==' ')
 		    {
-			board[4]='x';
+			board[4]=player;
 		    }
 		    else{
 		        System.out.println("Enetr at valid place");
@@ -118,7 +122,7 @@ public class TicTacToeGame {
 		case 5:
 		    if(board[5]==' ')
 		    {
-			board[5]='x';
+			board[5]=player;
 		    }
 		    else{
 		        System.out.println("Enetr at valid place");
@@ -127,7 +131,7 @@ public class TicTacToeGame {
 		case 6:
 		    if(board[6]==' ')
 		    {
-			board[6]='x';
+			board[6]=player;
 		    }
 		    else{
 		        System.out.println("Enetr at valid place");
@@ -136,7 +140,7 @@ public class TicTacToeGame {
 		case 7:
 		    if(board[7]==' ')
 		    {
-			board[7]='x';
+			board[7]=player;
 		    }
 		    else{
 		        System.out.println("Enetr at valid place");
@@ -145,7 +149,7 @@ public class TicTacToeGame {
 		case 8:
 		    if(board[8]==' ')
 		    {
-			board[8]='x';
+			board[8]=player;
 		    }
 		    else{
 		        System.out.println("Enetr at valid place");
@@ -154,7 +158,7 @@ public class TicTacToeGame {
 		case 9:
 		    if(board[9]==' ')
 		    {
-			board[9]='x';
+			board[9]=player;
 		    }
 		    else{
 		        System.out.println("Enetr at valid place");
@@ -163,18 +167,23 @@ public class TicTacToeGame {
 				System.out.println("Enetr valid place");
 		}
 		
-	}
-	public void checkToss() //checkToss to who play first
+		showBoard();
+		return location;
+		}
+		
+	public int checkToss() //checkToss to who play first
 	{
 	    Random random=new Random();
 	    int checktoss=random.nextInt(2)+1;
 	    if(checktoss==1)
 	    {
 	        System.out.println("User Start First");
+	        isUserPlay=true;  
 	    }
 	    else{
 	        System.out.println("Computer Start First");
 	    }
+	    return checktoss;
 	}
 	
 	public static void main(String[] args) {
@@ -183,9 +192,10 @@ public class TicTacToeGame {
 		tictactoe.playerChoice(); // method call
 		tictactoe.showBoard(); // showBoard Method call
 		tictactoe.moveToDesiredLocation(); //desired Location method call
-		tictactoe.checkDesiredLocationIsFree(); // method call to check Location is free
-		tictactoe.checkToss(); //CheckToss Method call
-		
+		while(location<10)
+		{
+		tictactoe.checkDesiredLocationIsFree(); //desired location method
+		}
 	}
-
 }
+
